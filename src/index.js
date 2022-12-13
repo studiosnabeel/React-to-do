46197edeee8d2308897dc29a88186b030d0f28e1
Nabeel Akbar <studiosnabeel@gmail.com>
2022-12-13 20:38:30 +0500

    Modify TodoList and TodoContainer files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 //component file
 import TodoContainer from './components/TodoContainer';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <TodoContainer />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
