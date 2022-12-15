10220f7ca466adb1f4b06beedfa72a98715fac58
Nabeel Akbar <studiosnabeel@gmail.com>
2022-12-15 13:08:05 +0500

    Change import path to functionBased folder

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 //component file
-import TodoContainer from './components/TodoContainer';
+import TodoContainer from './functionBased/components/TodoContainer';
 
-import './App.css';
+import './functionBased/App.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
